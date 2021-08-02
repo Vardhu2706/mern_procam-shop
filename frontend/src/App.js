d44c15ae440e892557d5c12366b519fb779671a3
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-08-02 21:10:08 +0530

    Initial Commit.

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+// Importing Helpers
+import React from "react";
+
+// Importing Components
+
+// Functional Component
+const App = () => {
+  return (
+    <>
+      <h1>Welcome To ProCam Shop</h1>
+    </>
+  );
+};
+
+// Default Export
+export default App;
