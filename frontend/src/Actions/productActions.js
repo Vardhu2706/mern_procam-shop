c039ff6d26ab2a2455f7984adad0e28c70019c67
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-08-09 11:21:23 +0530

    Created Redux store and productReducer.

---
+++ frontend/src/Actions/productActions.js
@@ -0,0 +1,10 @@
+import {
+  PRODUCT_LIST_FAIL,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_REQUEST,
+} from "../Constants/productConstants";
+
+// Action
+export const listProducts = () => {};
+
+// 9:20
