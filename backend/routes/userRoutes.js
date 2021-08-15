2a01cdbe5120e177d729e5ffd1d2c9b0c25b7777
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-08-15 15:35:16 +0530

    Created backend functionality to register a user and also encrypted the password.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
 import { protect } from "../Middleware/AuthMiddleware.js";
-import { authUser, getUserProfile } from "../Controllers/userController.js";
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from "../Controllers/userController.js";
 
 const router = express.Router();
 
+router.route("/").post(registerUser);
 router.post("/login", authUser);
 router.route("/profile").get(protect, getUserProfile);
 
