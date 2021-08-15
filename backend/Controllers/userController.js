a54a5952908f654b9838395bb3edfe4a9e2f8e82
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-08-15 12:37:41 +0530

    Generated a JSON webtoken for user based on user ID.

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler";
 import User from "../Models/userModel.js";
+import generateToken from "../Utils/GenerateToken.js";
 
 // @desc       Auth user & get a token
 // @route      POST /api/users/login
@@ -16,7 +17,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
