4eefd59d159b0ae5c594df3000fa67b99a1a244d
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-08-30 18:58:59 +0530

    Typos

--- backend/seeder.js
+++ backend/seeder.js
@@ -4,9 +4,9 @@ import colors from "colors";
 import users from "./data/user.js";
 import products from "./data/products_new.js";
 
-import User from "./models/userModel.js";
-import Product from "./models/productModel.js";
-import Order from "./models/orderModel.js";
+import User from "./Models/userModel.js";
+import Product from "./Models/productModel.js";
+import Order from "./Models/orderModel.js";
 
 import connectDB from "./config/db.js";
 
